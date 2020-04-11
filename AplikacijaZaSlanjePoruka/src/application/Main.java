7c3a8395eaa96b7e4654bc4963bf645ec293b6cd
Marko <mare.radovanovic90@gmail.com>
2020-04-11 20:36:25 +0200

    Dodat Space

--- AplikacijaZaSlanjePoruka/src/application/Main.java
+++ AplikacijaZaSlanjePoruka/src/application/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
 			//BorderPane root = new BorderPane();
 			Scena1 root= new Scena1();
 			Scene scene = new Scene(root,400,400);
